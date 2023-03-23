1d2edcf50bda2064920794b60367933870e7dfee
Muhammed-Rishan <rishan.k@codilar.com>
2023-03-23 10:44:59 +0530

    Db-using-Pdo

--- Pdo/login.php
+++ Pdo/login.php
@@ -1,9 +1,9 @@
 <?php 
 session_start();
 
-if (isset($_SESSION[ 'admin'])) {
+if (isset($_SESSION[ 'username'])) {
     header('Location: dashboard.php');
-    exit;
+    exit();
 }
 $error_message = '';
 if (isset($_POST['submit'])) {
